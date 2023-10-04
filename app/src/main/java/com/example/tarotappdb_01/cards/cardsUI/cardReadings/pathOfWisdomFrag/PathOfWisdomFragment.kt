69723e9d15e96aa7825451585653f7a506cb78d9
drAgon-235 <d.slawo@gmail.com>
2023-10-04 18:38:22 +0200

    Commit_020: weitere Experimente mit Animationen - YoYo entdeckt :-D

--- app/src/main/java/com/example/tarotappdb_01/cards/cardsUI/cardReadings/pathOfWisdomFrag/PathOfWisdomFragment.kt
+++ app/src/main/java/com/example/tarotappdb_01/cards/cardsUI/cardReadings/pathOfWisdomFrag/PathOfWisdomFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.example.tarotappdb_01.R
+import com.example.tarotappdb_01.cards.cardsUI.CardsViewModel
 import com.example.tarotappdb_01.databinding.FragmentPathOfWisdomBinding
 import com.example.tarotappdb_01.databinding.FragmentSimplPathBinding
 
@@ -13,6 +15,8 @@ import com.example.tarotappdb_01.databinding.FragmentSimplPathBinding
 class PathOfWisdomFragment : Fragment() {
 
     private lateinit var binding: FragmentPathOfWisdomBinding
+    private val viewmodel: CardsViewModel by activityViewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +35,14 @@ class PathOfWisdomFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val rotatingCard = binding.backCardIV
+        binding.layCrossBTN.animate()
+
+        // Loading the simple, UNSHUFFLED List<Cards> into viewmodel's 'cardListSimple' (NO LiveData):
+        viewmodel.loadCardListFromDBinViewModel()
+
+
 
 
     }
--- app/src/main/java/com/example/tarotappdb_01/cards/cardsUI/cardReadings/simplePathFrag/simplPathFragment.kt
+++ app/src/main/java/com/example/tarotappdb_01/cards/cardsUI/cardReadings/simplePathFrag/simplPathFragment.kt
@@ -1,18 +1,34 @@
 package com.example.tarotappdb_01.cards.cardsUI.cardReadings.simplePathFrag
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.OnClickListener
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.Animation.AnimationListener
+import android.view.animation.AnimationUtils
+import androidx.core.view.ViewCompat.animate
+import androidx.fragment.app.activityViewModels
+import com.daimajia.androidanimations.library.Techniques
+import com.daimajia.androidanimations.library.YoYo
 import com.example.tarotappdb_01.R
+import com.example.tarotappdb_01.cards.cardsUI.CardsViewModel
+import com.example.tarotappdb_01.cards.model.Card
 import com.example.tarotappdb_01.databinding.FragmentReadingsHomeBinding
 import com.example.tarotappdb_01.databinding.FragmentSimplPathBinding
+import okhttp3.internal.SuppressSignatureCheck
+import okhttp3.internal.wait
 
 
 class simplPathFragment : Fragment() {
 
     private lateinit var binding: FragmentSimplPathBinding
+    private val viewmodel: CardsViewModel by activityViewModels()
+
+    //private var shuffledCardList: List<Card> = viewmodel.cardListSimple.shuffled()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,19 +43,67 @@ class simplPathFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        //shuffledCardList = viewmodel.cardListSimple.shuffled()
         binding = FragmentSimplPathBinding.inflate(inflater, container, false)
-        return binding.root      }
-
-
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val rotatingCard = binding.rotatingCardIV
+        val backCard = binding.backCardIV
+
+        // Loading the simple, UNSHUFFLED List<Cards> into viewmodel's 'cardListSimple' (NO LiveData):
+        viewmodel.loadCardListFromDBinViewModel()
+
+        var shuffledCardList: List<Card> = viewmodel.cardListSimple
+
+
+        binding.shuffleCross.setOnClickListener {
+            YoYo.with(Techniques.Shake).playOn(rotatingCard)
+            shuffledCardList = shuffledCardList.shuffled()
+        }
+
+
+
+        binding.layCrossBTN.setOnClickListener {
+
+            //YoYo.with(Techniques.BounceInDown).playOn(backCard)
+
+
+            var pic1 = shuffledCardList[0].picture
+            binding.card1IV.setImageResource(pic1)
+
+
+            var pic2 = shuffledCardList[1].picture
+            binding.card2IV.setImageResource(pic2)
+            var pic3 = shuffledCardList[2].picture
+            binding.cardIV.setImageResource(pic3)
+
+
+
+            //binding.card1IV.visibility = View.VISIBLE
+            //binding.back01IV.visibility = View.VISIBLE
+            //YoYo.with(Techniques.FadeInDown).playOn(binding.back01IV)
+            binding.card1IV.visibility = View.VISIBLE
+
+            YoYo.with(Techniques.StandUp).playOn(binding.card1IV)
+
+
+
+            binding.card2IV.visibility = View.VISIBLE
+            YoYo.with(Techniques.FadeInDown).playOn(binding.card2IV)
+            binding.cardIV.visibility = View.VISIBLE
+            YoYo.with(Techniques.FadeInDown).playOn(binding.cardIV)
+
+
+
+        }
+
 
 
     }
-
-
 
 
 }
