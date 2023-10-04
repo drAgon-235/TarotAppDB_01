655a9baa69ef2994516f613aaeed928a462cbb05
drAgon-235 <d.slawo@gmail.com>
2023-10-05 01:50:50 +0200

    Commit_021: große Fortschritte, einige Animationen sinnvoll und userfreundlich kombiniert (einige verworfen!!). Erstes Fragment (simpl_path) quasi fertig animiert.

--- app/src/main/java/com/example/tarotappdb_01/cards/cardsUI/cardReadings/simplePathFrag/simplPathFragment.kt
+++ app/src/main/java/com/example/tarotappdb_01/cards/cardsUI/cardReadings/simplePathFrag/simplPathFragment.kt
@@ -11,6 +11,7 @@ import android.view.animation.Animation
 import android.view.animation.Animation.AnimationListener
 import android.view.animation.AnimationUtils
 import androidx.core.view.ViewCompat.animate
+import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import com.daimajia.androidanimations.library.Techniques
 import com.daimajia.androidanimations.library.YoYo
@@ -56,51 +57,105 @@ class simplPathFragment : Fragment() {
 
         // Loading the simple, UNSHUFFLED List<Cards> into viewmodel's 'cardListSimple' (NO LiveData):
         viewmodel.loadCardListFromDBinViewModel()
+        var shuffledCardList: List<Card> =
+            viewmodel.cardListSimple  // is not shuffled yet for educational purposes
 
-        var shuffledCardList: List<Card> = viewmodel.cardListSimple
 
-
+        // Shuffeling Cards Button:
         binding.shuffleCross.setOnClickListener {
+            // First Animation: Shaking
             YoYo.with(Techniques.Shake).playOn(rotatingCard)
+            // Second Animation: Rotating up & down
+            rotatingCard.animate().apply {
+                duration = 400
+                rotationYBy(360f)
+            }.withEndAction {
+                rotatingCard.animate().apply {
+                    duration = 400
+                    rotationXBy(360f)
+                }
+            }
+            // NOW we are really shuffeling and using shuffled Card List:
             shuffledCardList = shuffledCardList.shuffled()
         }
 
 
 
+        // Laying Cards Button:
         binding.layCrossBTN.setOnClickListener {
 
             //YoYo.with(Techniques.BounceInDown).playOn(backCard)
 
-
+            // Connecting the pictures to the View (not visible yet), taking the shuffled cards from top (left) to bottom (right):
             var pic1 = shuffledCardList[0].picture
             binding.card1IV.setImageResource(pic1)
-
-
             var pic2 = shuffledCardList[1].picture
             binding.card2IV.setImageResource(pic2)
             var pic3 = shuffledCardList[2].picture
-            binding.cardIV.setImageResource(pic3)
+            binding.card03IV.setImageResource(pic3)
 
 
+            // Appearing-Animation Card 01 - A:
+            // CardBack comes faning in dowm...
+            binding.back01IV.visibility = View.VISIBLE
+            YoYo.with(Techniques.FadeInDown).playOn(binding.back01IV)
 
-            //binding.card1IV.visibility = View.VISIBLE
-            //binding.back01IV.visibility = View.VISIBLE
-            //YoYo.with(Techniques.FadeInDown).playOn(binding.back01IV)
-            binding.card1IV.visibility = View.VISIBLE
+            // Appearing-Animation Card 01 - B:
+            // CardBack turns by 90 degrees and later disappears...
+            binding.back01IV.animate().apply {
+                duration = 1500
+                this.rotationYBy(90f)
+                binding.card1IV.animate().rotationYBy(90f)
+            }.withEndAction {
+                // (CardBack disappears:)
+                binding.back01IV.isVisible = false
+                // ... now the 1st Card from deck appears visible and turns by 90 degrees
+                binding.card1IV.isVisible = true
+                binding.card1IV.animate().apply {
+                    duration = 1000
+                    this.rotationYBy(90f)
+                }
+            }
 
-            YoYo.with(Techniques.StandUp).playOn(binding.card1IV)
+            // Appearing-Animation Card 02 - A:
+            binding.back02IV.visibility = View.VISIBLE
+            YoYo.with(Techniques.FadeInDown).playOn(binding.back02IV)
+
+            // Appearing-Animation Card 02 - B:
+            binding.back02IV.animate().apply {
+                duration = 1500
+                this.rotationYBy(90f)
+                binding.card2IV.animate().rotationYBy(90f)
+            }.withEndAction {
+                binding.back02IV.isVisible = false
+                binding.card2IV.isVisible = true
+                binding.card2IV.animate().apply {
+                    duration = 1000
+                    this.rotationYBy(90f)
+                }
+            }
 
 
+            // Appearing-Animation Card 03 - A:
+            binding.back03IV.visibility = View.VISIBLE
+            YoYo.with(Techniques.FadeInDown).playOn(binding.back03IV)
 
-            binding.card2IV.visibility = View.VISIBLE
-            YoYo.with(Techniques.FadeInDown).playOn(binding.card2IV)
-            binding.cardIV.visibility = View.VISIBLE
-            YoYo.with(Techniques.FadeInDown).playOn(binding.cardIV)
-
+            // Appearing-Animation Card 03 - B:
+            binding.back03IV.animate().apply {
+                duration = 1500
+                this.rotationYBy(90f)
+                binding.card03IV.animate().rotationYBy(90f)
+            }.withEndAction {
+                binding.back03IV.isVisible = false
+                binding.card03IV.isVisible = true
+                binding.card03IV.animate().apply {
+                    duration = 1000
+                    this.rotationYBy(90f)
+                }
+            }
 
 
         }
-
 
 
     }
