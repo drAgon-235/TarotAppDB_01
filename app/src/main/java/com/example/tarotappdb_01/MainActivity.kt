a2dbd9db9ae0935aa5510d002b56d28058c81ed6
drAgon-235 <d.slawo@gmail.com>
2023-10-16 21:48:34 +0200

    Commit 30: Zwischendrin Projekt zerschossen und bei Commit 28 nochmal angefangen. (Nach einigen Problemen mit SDK Comfigurations Projekt vom Github-VC geholt und zum Laufen gebracht - voll der Act, noch vorher nie gemacht!!) Jetzt läuft die Toolbar, wie sie sollte. Zeigt den richtigen Label des Fragments an und navigiert immer korrekt zurück mit onSupportNavigateUp()

--- app/src/main/java/com/example/tarotappdb_01/MainActivity.kt
+++ app/src/main/java/com/example/tarotappdb_01/MainActivity.kt
@@ -3,19 +3,51 @@ package com.example.tarotappdb_01
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.tarotappdb_01.cards.cardsUI.CardsViewModel
+import com.example.tarotappdb_01.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewmodel: CardsViewModel
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: CardsViewModel
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        // Test 01
+    // The Boiler-Plate for implementing the Material Toolbar
+    //
+        // Get the navHostFragment:
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
-        //viewmodel = ViewModelProvider(this)[CardsViewModel::class.java]
+        // Get the navController from the navHostFragment:
+        navController = navHostFragment.navController
+
+        // get the MainViewModel:
+        viewModel = ViewModelProvider(this)[CardsViewModel::class.java]
+
+        // setting up the ActionTOOLBar with the navController:
+        setupActionBarWithNavController(navController)
+
     }
+
+
+    // making the Back-Arrow work properly:
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+
+
+
+
+
+
 }
